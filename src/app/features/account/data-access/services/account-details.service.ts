46177618e0a29b444ca60d3329b4b0ff91358a79
pavlokrykh <pavkrykh@gmail.com>
2025-06-17 16:44:46 +0300

    feat: improve loading state management in account details and update styles for better layout

--- src/app/features/account/data-access/services/account-details.service.ts
+++ src/app/features/account/data-access/services/account-details.service.ts
@@ -1,14 +1,25 @@
 import { inject, Injectable, signal } from '@angular/core';
 import { AccountDetailsClient } from '@core/api/dashboard/account-details.client';
 import { AccountDetails } from '@core/models/account/account-details.model';
+import { finalize, tap } from 'rxjs';
 
 @Injectable()
 export class AccountDetailsService {
   private readonly accountDetailsClient = inject(AccountDetailsClient);
 
+  readonly $isLoading = signal<boolean>(false);
   readonly $accountDetails = signal<AccountDetails | null>(null);
 
   getAccountDetails() {
-    return this.accountDetailsClient.getAccountDetails();
+    this.$isLoading.set(true);
+
+    return this.accountDetailsClient.getAccountDetails().pipe(
+      tap((data) => {
+        this.$accountDetails.set(data);
+      }),
+      finalize(() => {
+        this.$isLoading.set(false);
+      })
+    );
   }
 }
--- src/app/features/account/view/page/account.component.ts
+++ src/app/features/account/view/page/account.component.ts
@@ -2,7 +2,7 @@ import { AccountDetailsService } from '@account/data-access/services/account-det
 import { AlertCardComponent } from '@account/view/components/alert-card/alert-card.component';
 import { MetricCardComponent } from '@account/view/components/metric-card/metric-card.component';
 import { PoliciesListComponent } from '@account/view/components/policies-list/policies-list.component';
-import { Component, inject, signal } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 // eslint-disable-next-line max-len
 import { AccountStatusProgressComponent } from '@account/view/components/account-status-progress/account-status-progress.component';
@@ -12,6 +12,7 @@ import { CommunicationComponent } from '@account/view/components/communication/c
 import { ComplianceDocumentationComponent } from '@account/view/components/compliance-documentation/compliance-documentation.component';
 // eslint-disable-next-line max-len
 import { PoliciesListComponent as DashboardPoliciesListComponent } from '@dashboard/view/components/policies-list/policies-list.component';
+import { SkeletonDirective } from '@shared/directives/skeleton.directive';
 
 @Component({
   selector: 'app-account',
@@ -24,6 +25,7 @@ import { PoliciesListComponent as DashboardPoliciesListComponent } from '@dashbo
     AccountsDetailsComponent,
     CommunicationComponent,
     DashboardPoliciesListComponent,
+    SkeletonDirective,
   ],
   templateUrl: './account.component.html',
   styleUrls: ['./account.component.scss'],
@@ -32,21 +34,10 @@ import { PoliciesListComponent as DashboardPoliciesListComponent } from '@dashbo
 export class AccountComponent {
   private readonly accountDetailsService = inject(AccountDetailsService);
 
-  readonly $accountDetails = signal(this.accountDetailsService.$accountDetails());
+  readonly $isLoading = this.accountDetailsService.$isLoading;
+  readonly $accountDetails = this.accountDetailsService.$accountDetails;
 
   constructor() {
-    this.getAccountDetails();
-  }
-
-  private getAccountDetails() {
-    this.accountDetailsService
-      .getAccountDetails()
-      .pipe(takeUntilDestroyed())
-      .subscribe((data) => {
-        console.log('Account details fetched:', data);
-
-        this.accountDetailsService.$accountDetails.set(data);
-        this.$accountDetails.set(data);
-      });
+    this.accountDetailsService.getAccountDetails().pipe(takeUntilDestroyed()).subscribe();
   }
 }
